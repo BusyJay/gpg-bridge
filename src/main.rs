994b29bf2f70b42fb6251f0b476d319effe2b4d5
Jay Lee <busyjaylee@gmail.com>
2020-03-08 20:07:28 +0800

    Allow detach

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+use std::os::windows::process::CommandExt;
+use std::process::Command;
 use std::{env, io};
+
+fn print_help(program: &str) {
+    println!("Usage: {} addr-to-bind [path-to-gpg-extra-socket]", program);
+    println!();
+    println!("Add --detach flag to run it as a background daemon.");
+}
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
@@ -6,11 +14,47 @@ async fn main() -> io::Result<()> {
 
     let mut args = env::args();
     let program = args.next().unwrap();
-    let from = args.next().unwrap_or_else(|| "--help".to_owned());
-    if from == "--help" || from == "-h" {
-        println!("Usage: {} from-addr [path-to-gpg-extra-socket]", program);
-        return Ok(());
+
+    let mut from = String::new();
+    let mut to = None;
+    let mut detach = false;
+    for arg in args {
+        if arg == "--help" || arg == "-h" {
+            print_help(&program);
+            return Ok(());
+        }
+        if arg == "--detach" {
+            detach = true;
+            continue;
+        }
+        if arg.starts_with('-') || (!from.is_empty() && to.is_some()) {
+            return Err(io::Error::new(
+                io::ErrorKind::Other,
+                format!("unrecoginze arg {}", arg),
+            ));
+        }
+        if from.is_empty() {
+            from = arg;
+        } else {
+            to = Some(arg)
+        }
     }
-    let to = args.next();
-    gpg_bridge::bridge(from, to).await
+    if from.is_empty() {
+        return Err(io::Error::new(
+            io::ErrorKind::Other,
+            "You need to specify bind address",
+        ));
+    }
+    if !detach {
+        return gpg_bridge::bridge(from, to).await;
+    }
+
+    let mut cmd = Command::new(program);
+    cmd.arg(from);
+    if let Some(t) = to {
+        cmd.arg(t);
+    }
+    cmd.creation_flags(0x0000_0200 | 0x0000_0008 | 0x0400_0000)
+        .spawn()?;
+    Ok(())
 }
